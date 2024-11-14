9da65a2d7085e9bb08c08876a3bbf7b64d4b3134
Drozdi <drozdi-klim@yandex.ru>
2024-11-14 20:55:46 +0300

    -

--- src/pages/components/post-content.jsx
+++ src/pages/components/post-content.jsx
@@ -1,5 +1,4 @@
 import { useNavigate } from 'react-router-dom';
-import { XBtn } from '../../components/ui';
 import { SpecialPanel } from './special-panel';
 export function PostContent({
 	className,
@@ -10,17 +9,11 @@ export function PostContent({
 	return (
 		<div className={className}>
 			<h2 className="font-bold text-2xl my-10">{title}</h2>
-			<img src={image_url} alt={title} />
+			<img className="float-left mr-4" src={image_url} alt={title} />
 			<SpecialPanel
 				className="-mt-5 mx-0 mb-5"
 				id={id}
 				publishedAt={published_at}
-				editButton={
-					<XBtn
-						icon="mdi-pencil-box-outline"
-						onClick={() => navigate(`/post/${id}/edit`)}
-					/>
-				}
 			/>
 			<div className="text-lg whitespace-pre-line">{content}</div>
 		</div>
--- src/pages/components/special-panel.jsx
+++ src/pages/components/special-panel.jsx
@@ -1,9 +1,25 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { XBtn, XBtnGroup, XIcon } from '../../components/ui';
+import { ROLE } from '../../constants';
 import { selectUserRole } from '../../selectors';
-export function SpecialPanel() {
+export function SpecialPanel({ publishedAt, onPostRemove, id }) {
 	const dispatch = useDispatch();
 	const navigate = useNavigate();
 	const roleId = useSelector(selectUserRole);
-	return <></>;
+	const isAdmin = roleId === ROLE.ADMIN;
+	return (
+		<div className="flex justify-between items-center">
+			<div>
+				{publishedAt && <XIcon className="mr-2">mdi-calendar-text-outline</XIcon>}
+				{publishedAt}
+			</div>
+			{isAdmin && (
+				<XBtnGroup round>
+					<XBtn icon="mdi-pencil-box-outline" to={`/post/${id}/edit`} />
+					{publishedAt && <XBtn icon="mdi-delete" />}
+				</XBtnGroup>
+			)}
+		</div>
+	);
 }
